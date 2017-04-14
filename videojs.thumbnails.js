316710a2d5dbb35582755cd901042ce43702b04a
Wolfgang Breyha <wbreyha@gmx.net>
2017-04-14 20:44:59 +0200

    video.js 5.19 version initial checkin

--- videojs.thumbnails.js
+++ videojs.thumbnails.js
@@ -67,94 +67,31 @@
    */
   videojs.plugin('thumbnails', function(options) {
     var div, settings, img, player, progressControl, duration, moveListener, moveCancel, thumbTrack;
-    defaults.basePath = options.basePath || defaults.basePath;
     settings = extend({}, defaults, options);
     player = this;
-    //detect which track we use. For now we just use the first metadata track
-    var numtracks = player.textTracks().length;
-    if (numtracks === 0) {
+
+    if (!options) {
       return;
     }
-    i = 0;
-    while (i<numtracks) {
-      if (player.textTracks()[i].kind==='metadata') {
-        thumbTrack = player.textTracks()[i];
-        //Chrome needs this
-        thumbTrack.mode = 'hidden';
-        break;
-      }
-      i++;
+    var player = this;
+    if (!options.vtt) {
+      return;
     }
-    (function() {
-      var progressControl, addFakeActive, removeFakeActive;
-      // Android doesn't support :active and :hover on non-anchor and non-button elements
-      // so, we need to fake the :active selector for thumbnails to show up.
-      if (navigator.userAgent.toLowerCase().indexOf("android") !== -1) {
-        progressControl = player.controlBar.progressControl;
+    defaults.basePath = options.basePath || options.vtt.substring(0, options.vtt.lastIndexOf('/') + 1);;
+    console.log(defaults.basePath);
 
-        addFakeActive = function() {
-          progressControl.addClass('fake-active');
-        };
-        removeFakeActive = function() {
-          progressControl.removeClass('fake-active');
-        };
-
-        progressControl.on('touchstart', addFakeActive);
-        progressControl.on('touchend', removeFakeActive);
-        progressControl.on('touchcancel', removeFakeActive);
-      }
-    })();
-
-    // create the thumbnail
-    div = document.createElement('div');
-    div.className = 'vjs-thumbnail-holder';
-    img = document.createElement('img');
-    div.appendChild(img);
-    img.className = 'vjs-thumbnail';
-
-    // keep track of the duration to calculate correct thumbnail to display
-    duration = player.duration();
-    
     // when the container is MP4
     player.on('durationchange', function(event) {
       duration = player.duration();
     });
 
-    // when the container is HLS
-    player.on('loadedmetadata', function(event) {
-      duration = player.duration();
-    });
-
-    // add the thumbnail to the player
-    progressControl = player.controlBar.progressControl;
-    progressControl.el().appendChild(div);
-
     moveListener = function(event) {
       var mouseTime, time, active, left, setting, pageX, right, width, halfWidth, pageXOffset, clientRect;
       active = 0;
-      pageXOffset = getScrollOffset().x;
-      clientRect = offsetParent(progressControl.el()).getBoundingClientRect();
-      right = (clientRect.width || clientRect.right) + pageXOffset;
-
-      pageX = event.pageX;
-      if (event.changedTouches) {
-        pageX = event.changedTouches[0].pageX;
-      }
-
-      // find the page offset of the mouse
-      left = pageX || (event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft);
-      // subtract the page offset of the positioned offset parent
-      left -= offsetParent(progressControl.el()).getBoundingClientRect().left + pageXOffset;
-
-      // apply updated styles to the thumbnail if necessary
-      // mouseTime is the position of the mouse along the progress control bar
-      // `left` applies to the mouse position relative to the player so we need
-      // to remove the progress control's left offset to know the mouse position
-      // relative to the progress control
-      mouseTime = Math.floor((left - progressControl.el().offsetLeft) / progressControl.width() * duration);
+      mouseTime = player.controlBar.progressControl.seekBar.calculateDistance(event) * duration;
 
       //Now check which of the cues applies
-      var cnum = thumbTrack&&thumbTrack.cues.length;
+      var cnum = thumbTrack && thumbTrack.cues.length;
       i = 0;
       while (i<cnum) {
         var ccue = thumbTrack.cues[i];
@@ -183,19 +120,34 @@
       }
 
       //Set the container width/height if it changed
-      if (div.style.width != setting.w || div.style.height != setting.h) {
-        div.style.width = setting.w + 'px';
-        div.style.height = setting.h + 'px';
+      var padding = 2;
+      var textwidth = 60;
+
+      var compareh = (setting.h + padding*2) + 'px';
+      var comparew = (setting.w + padding*2) + 'px';
+      if (div.style.width != comparew || div.style.height != compareh) {
+        div.style.width = (setting.w + padding*2) + 'px';
+        div.style.height = (setting.h + padding*2) + 'px';
+        div.style.top = -(setting.h + 15) + 'px';
+	if (padding > 0) {
+          div.style.border = padding + 'px solid rgba(0,0,0,0.9)';
+	}
+	//div.style.position = 'relative';
+        span.style.width = textwidth + 'px';
+        span.style.left = (setting.w/2 - textwidth/2) + 'px';
       }
       //Set the image cropping
       img.style.left = -(setting.x) + 'px';
-      img.style.top = -(setting.y) + 'px';
+      img.style.top  = -(setting.y) + 'px';
       img.style.clip = 'rect('+setting.y+'px,'+(setting.w+setting.x)+'px,'+(setting.y+setting.h)+'px,'+setting.x+'px)';
-      
+
+      var timeStr = new Date(1000 * mouseTime).toISOString().substr(11, 8);
+      span.innerHTML = timeStr;
+
       width = setting.w;
       halfWidth = width / 2;
 
-      // make sure that the thumbnail doesn't fall off the right side of the left side of the player
+/*      // make sure that the thumbnail doesn't fall off the right side of the left side of the player
       if ( (left + halfWidth) > right ) {
         left = right - width;
       } else if (left < halfWidth) {
@@ -203,22 +155,87 @@
       } else {
         left = left - halfWidth;
       }
+*/
+      div.style.left = -(halfWidth) + 'px';
 
-      div.style.left = left + 'px';
     };
+
+    moveCancel = function(event) {
+      //console.log(event);
+      if (event.explicitOriginalTarget) {
+          console.log(event.explicitOriginalTarget.className); }
+
+      if ((! event.explicitOriginalTarget) || 
+          ((event.explicitOriginalTarget.ClassName !== 'vjs-mouse-display') &&
+          (event.explicitOriginalTarget.ClassName !== 'vjs-progress-holder vjs-slider vjs-slider-horizontal')))  {
+      console.log("done");
+      div.style.left = '-1000px'; }
+    };
+
+    player.ready(function() {
+
+    var trackEl = player.addRemoteTextTrack({
+      id: 'thumbnails',
+      kind: 'metadata',
+      src: options.vtt,
+    });
+    trackEl.addEventListener('load', function onLoad() {
+      trackEl.removeEventListener('load', onLoad);
+      thumbTrack = player.textTracks().getTrackById('thumbnails');
+    });
+
+    (function() {
+      var progressControl, addFakeActive, removeFakeActive;
+      // Android doesn't support :active and :hover on non-anchor and non-button elements
+      // so, we need to fake the :active selector for thumbnails to show up.
+      if (navigator.userAgent.toLowerCase().indexOf("android") !== -1) {
+        progressControl = player.controlBar.progressControl;
+
+        addFakeActive = function() {
+          progressControl.addClass('fake-active');
+        };
+        removeFakeActive = function() {
+          progressControl.removeClass('fake-active');
+        };
+
+//        progressControl.on('touchstart', addFakeActive);
+//        progressControl.on('touchend', removeFakeActive);
+//        progressControl.on('touchcancel', removeFakeActive);
+      }
+    })();
+
+    // create the thumbnail
+    div = document.createElement('div');
+    div.className = 'vjs-thumbnail-holder';
+    img = document.createElement('img');
+    div.appendChild(img);
+    img.className = 'vjs-thumbnail';
+    span = document.createElement('span');
+    div.appendChild(span);
+    span.className = 'vjs-thumbnail-text';
+
+    // keep track of the duration to calculate correct thumbnail to display
+    duration = player.duration();
+    
+    // add the thumbnail to the player
+    progressControl = player.controlBar.progressControl;
+    //progressControl.el().appendChild(div);
+
+    MTD = player.controlBar.progressControl.seekBar.mouseTimeDisplay;
+    MTD.el().appendChild(div);
+    //MTD.on('mousemove', MTD_moveListener);
 
     // update the thumbnail while hovering
     progressControl.on('mousemove', moveListener);
     progressControl.on('touchmove', moveListener);
 
-    moveCancel = function(event) {
-      div.style.left = '-1000px';
-    };
-
     // move the placeholder out of the way when not hovering
-    progressControl.on('mouseout', moveCancel);
-    progressControl.on('touchcancel', moveCancel);
+/*    progressControl.on('mouseout', moveCancel);
+*/    progressControl.on('touchcancel', moveCancel);
     progressControl.on('touchend', moveCancel);
     player.on('userinactive', moveCancel);
+
+    }, true);
+
   });
 })();
